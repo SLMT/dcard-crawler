b187ac74b1c5e5f45ed98b5d1fdec429fe6272d6
slmt <sam123456777@gmail.com>
2016-05-14 17:49:51 +0800

    Add command line options and comment some error code

--- src/main/java/slmt/crawler/dcard/CSVConverter.java
+++ src/main/java/slmt/crawler/dcard/CSVConverter.java
@@ -21,151 +21,151 @@ import com.alibaba.fastjson.JSON;
 
 public class CSVConverter {
 
-	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat(
-			"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-
-	private static String inputDirName;
-	private static String outputDirName;
-
-	public static void main(String[] args) {
-
-		// Check the number of arguments
-		if (args.length < 2) {
-			System.out.println("Arguments: [Input Dir] [Output Dir]");
-			return;
-		}
-
-		// Get arguments
-		inputDirName = args[0];
-		outputDirName = args[1];
-
-		try {
-			// Timer
-			long totalStart = System.nanoTime();
-
-			// Read the list of input files
-			File inputDir = new File(inputDirName);
-			String[] inputFiles = inputDir.list();
-
-			// Create output files
-			BufferedWriter postOut = new BufferedWriter(new OutputStreamWriter(
-					new FileOutputStream(new File(outputDirName, "posts.csv")),
-					"UTF-8"));
-			BufferedWriter comOut = new BufferedWriter(new OutputStreamWriter(
-					new FileOutputStream(
-							new File(outputDirName, "comments.csv")), "UTF-8"));
-
-			// Convert files
-			for (String fileName : inputFiles) {
-				String idPart = fileName.substring(0, 5);
-				String typePart = fileName.substring(6);
-
-				if (typePart.equals("json")) {
-					long start = System.nanoTime();
-					String json = loadFile(new File(inputDirName, fileName));
-					int postId = Integer.parseInt(idPart);
-
-					Post post = JSON.parseObject(json, Post.class);
-					CSVPost csvPost = convertToCSV(postId, post);
-					saveToFile(csvPost, postOut, comOut);
-
-					long time = System.nanoTime() - start;
-					System.out.println("Process post." + postId + " took "
-							+ time / 1000000 + "ms.");
-				}
-			}
-
-			// Close files
-			postOut.close();
-			comOut.close();
-
-			// Timer end
-			long totalTime = System.nanoTime() - totalStart;
-			System.out.println("Whole program took " + totalTime / 1000000
-					+ "ms.");
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static void saveToFile(CSVPost post, BufferedWriter postOut,
-			BufferedWriter comOut) throws IOException {
-		// Write post
-		postOut.append(post.toString());
-		postOut.newLine();
-
-		// Write comments
-		for (CSVComment com : post.comments) {
-			comOut.append(com.toString());
-			comOut.newLine();
-		}
-	}
-
-	private static CSVPost convertToCSV(int postId, Post jsonPost) {
-		CSVPost post = new CSVPost();
-
-		try {
-			// Convert post
-			post.pid = postId;
-			post.fourm = jsonPost.forum_alias;
-			post.pinned = jsonPost.pinned;
-			post.reply = jsonPost.reply;
-			post.createdAt = getDateInLong(jsonPost.createdAt);
-			post.updatedAt = getDateInLong(jsonPost.updatedAt);
-			post.likeCount = jsonPost.likeCount;
-
-			post.posterGender = jsonPost.member.gender;
-			post.posterSchool = jsonPost.member.school;
-			post.posterDepart = jsonPost.member.department;
-			post.anonymousSchool = jsonPost.anonymousSchool;
-			post.anonymousDepartment = jsonPost.anonymousDepartment;
-
-			Version v = jsonPost.version.get(0);
-			post.title = v.title.replace("\"", "\"\""); // for CSV format
-			post.content = v.content.replace("\"", "\"\""); // for CSV format
-
-			// Convert comments
-			int cid = 1;
-			for (Comment jsonComment : jsonPost.comment) {
-				CSVComment comment = new CSVComment();
-
-				comment.cid = cid++;
-				comment.postId = postId;
-				comment.like = jsonComment.like;
-				comment.hidden = jsonComment.hidden;
-				comment.posterGender = jsonComment.member.gender;
-				comment.posterSchool = jsonComment.member.school;
-				comment.posterDepart = jsonComment.member.department;
-				comment.anonymous = jsonComment.anonymous;
-				comment.host = jsonComment.host;
-
-				v = jsonComment.version.get(0);
-				comment.content = v.content.replace("\"", "\"\""); // for CSV
-																	// format
-				comment.postTime = getDateInLong(v.createdAt);
-
-				post.comments.add(comment);
-			}
-
-		} catch (RuntimeException e) {
-			e.printStackTrace();
-			System.out.println("Post " + postId + " parse failed.");
-		}
-
-		return post;
-	}
-
-	private static long getDateInLong(String dateString) {
-		try {
-			return DATE_FORMAT.parse(dateString).getTime();
-		} catch (ParseException e) {
-			try {
-				return Long.parseLong(dateString);
-			} catch (NumberFormatException ex) {
-				throw new RuntimeException("Parse date: " + dateString
-						+ " failed.");
-			}
-		}
-	}
+//	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat(
+//			"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+//
+//	private static String inputDirName;
+//	private static String outputDirName;
+//
+//	public static void main(String[] args) {
+//
+//		// Check the number of arguments
+//		if (args.length < 2) {
+//			System.out.println("Arguments: [Input Dir] [Output Dir]");
+//			return;
+//		}
+//
+//		// Get arguments
+//		inputDirName = args[0];
+//		outputDirName = args[1];
+//
+//		try {
+//			// Timer
+//			long totalStart = System.nanoTime();
+//
+//			// Read the list of input files
+//			File inputDir = new File(inputDirName);
+//			String[] inputFiles = inputDir.list();
+//
+//			// Create output files
+//			BufferedWriter postOut = new BufferedWriter(new OutputStreamWriter(
+//					new FileOutputStream(new File(outputDirName, "posts.csv")),
+//					"UTF-8"));
+//			BufferedWriter comOut = new BufferedWriter(new OutputStreamWriter(
+//					new FileOutputStream(
+//							new File(outputDirName, "comments.csv")), "UTF-8"));
+//
+//			// Convert files
+//			for (String fileName : inputFiles) {
+//				String idPart = fileName.substring(0, 5);
+//				String typePart = fileName.substring(6);
+//
+//				if (typePart.equals("json")) {
+//					long start = System.nanoTime();
+//					String json = loadFile(new File(inputDirName, fileName));
+//					int postId = Integer.parseInt(idPart);
+//
+//					Post post = JSON.parseObject(json, Post.class);
+//					CSVPost csvPost = convertToCSV(postId, post);
+//					saveToFile(csvPost, postOut, comOut);
+//
+//					long time = System.nanoTime() - start;
+//					System.out.println("Process post." + postId + " took "
+//							+ time / 1000000 + "ms.");
+//				}
+//			}
+//
+//			// Close files
+//			postOut.close();
+//			comOut.close();
+//
+//			// Timer end
+//			long totalTime = System.nanoTime() - totalStart;
+//			System.out.println("Whole program took " + totalTime / 1000000
+//					+ "ms.");
+//
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
+//
+//	private static void saveToFile(CSVPost post, BufferedWriter postOut,
+//			BufferedWriter comOut) throws IOException {
+//		// Write post
+//		postOut.append(post.toString());
+//		postOut.newLine();
+//
+//		// Write comments
+//		for (CSVComment com : post.comments) {
+//			comOut.append(com.toString());
+//			comOut.newLine();
+//		}
+//	}
+//
+//	private static CSVPost convertToCSV(int postId, Post jsonPost) {
+//		CSVPost post = new CSVPost();
+//
+//		try {
+//			// Convert post
+//			post.pid = postId;
+//			post.fourm = jsonPost.forum_alias;
+//			post.pinned = jsonPost.pinned;
+//			post.reply = jsonPost.reply;
+//			post.createdAt = getDateInLong(jsonPost.createdAt);
+//			post.updatedAt = getDateInLong(jsonPost.updatedAt);
+//			post.likeCount = jsonPost.likeCount;
+//
+//			post.posterGender = jsonPost.member.gender;
+//			post.posterSchool = jsonPost.member.school;
+//			post.posterDepart = jsonPost.member.department;
+//			post.anonymousSchool = jsonPost.anonymousSchool;
+//			post.anonymousDepartment = jsonPost.anonymousDepartment;
+//
+//			Version v = jsonPost.version.get(0);
+//			post.title = v.title.replace("\"", "\"\""); // for CSV format
+//			post.content = v.content.replace("\"", "\"\""); // for CSV format
+//
+//			// Convert comments
+//			int cid = 1;
+//			for (Comment jsonComment : jsonPost.comment) {
+//				CSVComment comment = new CSVComment();
+//
+//				comment.cid = cid++;
+//				comment.postId = postId;
+//				comment.like = jsonComment.like;
+//				comment.hidden = jsonComment.hidden;
+//				comment.posterGender = jsonComment.member.gender;
+//				comment.posterSchool = jsonComment.member.school;
+//				comment.posterDepart = jsonComment.member.department;
+//				comment.anonymous = jsonComment.anonymous;
+//				comment.host = jsonComment.host;
+//
+//				v = jsonComment.version.get(0);
+//				comment.content = v.content.replace("\"", "\"\""); // for CSV
+//																	// format
+//				comment.postTime = getDateInLong(v.createdAt);
+//
+//				post.comments.add(comment);
+//			}
+//
+//		} catch (RuntimeException e) {
+//			e.printStackTrace();
+//			System.out.println("Post " + postId + " parse failed.");
+//		}
+//
+//		return post;
+//	}
+//
+//	private static long getDateInLong(String dateString) {
+//		try {
+//			return DATE_FORMAT.parse(dateString).getTime();
+//		} catch (ParseException e) {
+//			try {
+//				return Long.parseLong(dateString);
+//			} catch (NumberFormatException ex) {
+//				throw new RuntimeException("Parse date: " + dateString
+//						+ " failed.");
+//			}
+//		}
+//	}
 }
