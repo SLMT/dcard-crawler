3c681e7453c658897452a14c4462606193bde36c
slmt <sam123456777@gmail.com>
2016-05-14 16:57:19 +0800

    Add more forums

--- src/main/java/slmt/crawler/dcard/api/DcardForum.java
+++ src/main/java/slmt/crawler/dcard/api/DcardForum.java
@@ -1,16 +1,24 @@
 package slmt.crawler.dcard.api;
 
+/**
+ * Dcard 的子論壇名稱。目前主要是用來當作參數傳遞，以及提取別名 (alias)。
+ * 注意 CCC = 3C，因為變數名稱不能是數字開頭 。
+ * 
+ * @author SLMT
+ *
+ */
 public enum DcardForum {
-	ALL, SEX;
+	ALL, BOOK, ACG, GAME, CCC, FUNNY, BG, TREADING, TALK, GIRL, BOY,
+	MOOD, MUSIC, TRAVEL, PHOTOGRAPHY, MOVIE, HOROSCOPES, LITERATURE, SPORT,
+	PET, FOOD, HANDICRAFTS, JOB, STUDYABROAD, MARVEL, FRESHMAN, COURSE,
+	EXAM, SEX, DCARD, WHYSOSERIOUS;
+	
+	public static DcardForum getByName(String name) {
+		name = name.toUpperCase();
+		return DcardForum.valueOf(name);
+	}
 	
 	public String getAlias() {
-		switch (this) {
-		case ALL:
-			return "all";
-		case SEX:
-			return "sex";
-		default:
-			return null;
-		}
+		return this.name().toLowerCase();
 	}
 }
