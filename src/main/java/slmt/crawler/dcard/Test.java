466b70267c660af0116b1112be893f6ea1481659
SLMT <sam123456777@gmail.com>
2016-05-22 18:09:12 +0800

    Quick test all actions

--- src/main/java/slmt/crawler/dcard/Test.java
+++ src/main/java/slmt/crawler/dcard/Test.java
@@ -9,6 +9,7 @@ public class Test {
 
 	public static void main(String[] args) throws IOException {
 		Action action = new TopAction();
+		action.execute(new String[]{"fetch-post", "-e", "-f", "PHOTOGRAPHY", "10", "/tmp/posts"});
 		action.execute(new String[]{"fetch-image", "/tmp/posts", "/tmp/posts"});
 	}
 
