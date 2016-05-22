d3ab5c46cdb0a7f4a72087060305b45c8242b8c6
SLMT <sam123456777@gmail.com>
2016-05-22 18:20:04 +0800

    Update the version number

--- src/main/java/slmt/crawler/dcard/DcardCrawler.java
+++ src/main/java/slmt/crawler/dcard/DcardCrawler.java
@@ -5,7 +5,7 @@ import slmt.crawler.dcard.action.TopAction;
 
 public class DcardCrawler {
 	
-	public static final String VERSION = "0.1.0";
+	public static final String VERSION = "0.2.0";
 
 	public static void main(String[] args) {
 		Action top = new TopAction();
