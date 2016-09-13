a10e4c6780ac09ffd4f7fa2e37f6e70447b54d64
SLMT <sam123456777@gmail.com>
2016-09-13 10:46:56 +0800

    Fix issue #1 (bug)

--- src/main/java/slmt/crawler/dcard/Test.java
+++ src/main/java/slmt/crawler/dcard/Test.java
@@ -1,16 +1,20 @@
 package slmt.crawler.dcard;
 
 import java.io.IOException;
+import java.util.List;
 
-import slmt.crawler.dcard.action.Action;
-import slmt.crawler.dcard.action.TopAction;
+import slmt.crawler.dcard.api.DcardForumAPI;
+import slmt.crawler.dcard.json.PostInfo;
 
 public class Test {
 
 	public static void main(String[] args) throws IOException {
-		Action action = new TopAction();
-		action.execute(new String[]{"fetch-post", "-e", "-f", "PHOTOGRAPHY", "10", "/tmp/posts"});
-		action.execute(new String[]{"fetch-image", "/tmp/posts", "/tmp/posts"});
+//		Action action = new TopAction();
+//		action.execute(new String[]{"fetch-post", "-e", "-f", "PHOTOGRAPHY", "10", "/tmp/posts"});
+//		action.execute(new String[]{"fetch-image", "/tmp/posts", "/tmp/posts"});
+		
+		List<PostInfo> list = DcardForumAPI.getPostList();
+		System.out.println(list);
 	}
 
 }
